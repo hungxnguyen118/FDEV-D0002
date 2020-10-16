48284b671e5fece6deb89ec8fbf8a47ee7182472
Hung Nguyen <nxhung118@gmail.com>
2020-10-16 18:46:16 +0700

    upload all image

---
+++ do_an_nho_nho/index.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_GET['page'])){
+    if($_GET['page'] == 'sach'){
+        include_once('./pages/ds_sach.php');
+    }
+    else{
+        include_once('./pages/trang_chu.php');
+    }
+}
+else {
+    include_once('./pages/trang_chu.php');
+}
+
+
+?>
