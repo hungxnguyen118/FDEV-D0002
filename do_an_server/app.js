79c35b800b7c538dc03a4a5bd87657693e03cbf0
Hung Nguyen <nxhung118@gmail.com>
2020-12-23 21:15:09 +0700

    Middleware

--- do_an_server/app.js
+++ do_an_server/app.js
@@ -3,12 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var userRouter = require('./routes/user');
 
 var app = express();
+
+var complete_log = (req, res, next) => {
+  try {
+      //console.log(hahaha.length);
+      var string_log = '-200-' + JSON.stringify({
+          'xu_ly': 'thêm user mới',
+          data_send: req.body
+      }) +  '\n';
+  
+      fs.appendFileSync('./data_log/2020_12_23.log', string_log);
+  }
+  catch(err){
+      var string_log = '-500-Server Internal Error' +  '\n';
+      fs.appendFileSync('./data_log/2020_12_23.log', string_log);
+  }
+
+  next();
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +38,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  //console.log(Date.now() + '-' + req.method + '-' + req.url);
+  var string_log = Date.now() + '-' + req.method + '-' + req.url + '\n';
+  fs.appendFileSync('./data_log/2020_12_23.log', string_log);
+  next();
+})
+
+app.use(complete_log);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,7 +65,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send("Server Error")
+  //res.render('error');
 });
 
 module.exports = app;
--- do_an_server/routes/user.js
+++ do_an_server/routes/user.js
@@ -1,5 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+var authenticate = (req, res, next) => {
+    var authorization = req.header('Authorization');
+    var array_auth = authorization.split(' ');
+
+    //console.log(array_auth[1]);
+    var string_basic_auth = array_auth[1];
+    var data_string_auth = (new Buffer(string_basic_auth, 'base64')).toString();
+    // console.log(data_string_auth);
+
+    var user_info = data_string_auth.split(':');
+    if(user_info[0] == 'hungnguyen' && user_info[1] == '123456'){
+        next();
+    }
+    else{
+        res.status(401);
+        res.json({
+            'error': true,
+            'error_message': 'You don\'t have permission'
+        });
+    }
+}
 
 router.get('/', (req, res) => {
     res.json({'xu_ly': 'thông tin user'});
@@ -9,11 +32,19 @@ router.get('/:id_user', (req, res) => {
     res.json({'xu_ly': 'thông tin user ' + req.params.id_user});
 });
 
-router.post('/', (req, res) => {
+router.post('/', authenticate, (req, res) => {
+    console.log(hahaha.length);
     res.json({
         'xu_ly': 'thêm user mới',
         data_send: req.body
     });
 });
+
+router.post('/sign-up', (req, res) => {
+    res.json({
+        'xu_ly': 'đăng ký user mới',
+        data_send: req.body
+    });
+})
 
 module.exports = router;
