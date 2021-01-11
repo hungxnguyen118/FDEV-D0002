98bfdd21c9694ef95a72c5db7a1c22b05f3ecdc0
Hung Nguyen <nxhung118@gmail.com>
2021-01-11 21:28:48 +0700

    socket io part 2

--- do_an_react/src/Module/FormChat/index.js
+++ do_an_react/src/Module/FormChat/index.js
@@ -4,42 +4,44 @@ import axios from 'axios';
 
 const ENDPOINT = "http://localhost:4000/";
 
-function PopupChat() {
-  const [list_message, setListMessage] = useState([]);
+const socket = socketIOClient(ENDPOINT);
 
-  const [state_show_or_hide, setStateShowOrHide] = useState(0);
+function PopupChat() {
+
   const [state_style, setStateStyle] = useState({height: "0px"});
 
   const [input_message, setInputMessage] = useState('');
 
-  const socket = socketIOClient(ENDPOINT);
+  const [list_message, setListMessage] = useState([]);
+
+  const [message_server, setMessageServer] = useState('chưa có message');
 
   useEffect(() => {
 
-    axios.get('/danh-sach-chat')
+    axios.get('http://localhost:4000/messages')
       .then((results) => {
 
-        console.log(results);
+        console.log(results.data.data_send);
 
-        setListMessage(results.data);
+        setListMessage(results.data.data_send);
 
       })
       .catch((err) => {
         console.log(err);
       })
 
-
-    socket.on("MessageServer", data => {
+      socket.on('MessageFromServer', (data) => {
         console.log(data);
-        
+        //setMessageServer(data);
         setListMessage(list_message => [...list_message, data]);
-    });
+      });
     
   }, []);
 
   const handleSendMessageToServer = (e) => {
     e.preventDefault();
-    socket.emit("MessageClient", input_message);
+    socket.emit("MessageFromClient", input_message);
+    //setListMessage(list_message => [...list_message, input_message]);
   }
 
   const handleChangeInput = (e) => {
@@ -47,27 +49,18 @@ function PopupChat() {
   }
 
   const handleShowHidePopupChat = (e) => {
-    // if(state_show_or_hide == 0){
-    //   setStateShowOrHide(1);
+
+    // console.log(state_style.height);
+    // if(state_style.height == "0px"){
+    //   setStateStyle({
+    //     height: "358px"
+    //   })
     // }
     // else{
-    //   setStateShowOrHide(0);
+    //   setStateStyle({
+    //     height: "0px"
+    //   })
     // }
-
-    //setStateShowOrHide(!state_show_or_hide);
-    //setStateShowOrHide(1 - state_show_or_hide);
-
-    console.log(state_style.height);
-    if(state_style.height == "0px"){
-      setStateStyle({
-        height: "358px"
-      })
-    }
-    else{
-      setStateStyle({
-        height: "0px"
-      })
-    }
 
   }
 
@@ -75,51 +68,21 @@ function PopupChat() {
   return (
     <div className="div_include_chat_box">
 
-        {/* {
-          (state_show_or_hide == false)?
-            <div className="title_chat_form" onClick={handleShowHidePopupChat}>
-              Liên hệ chúng tôi
-            </div>
-          :
-            <div>
-                <div className="title_chat_form" onClick={handleShowHidePopupChat}>
-                  Liên hệ chúng tôi
-                </div>
-        
-                <div className="detect_an_hien">
-                  <div className="danh_sach_chat">
-                      {list_message.map((item_chat, index) => (
-                          <div key={index} className="item_chat">
-                              {item_chat.noi_dung}
-                          </div>
-                      ))}
-                  </div>
-        
-                  <div>
-                      
-                      <form onSubmit={handleSendMessageToServer}>
-                          <input className="input_message" onChange={handleChangeInput} type="text" name="message" value={input_message} />
-                          <button className="button_send_message" type="submit">
-                            <img src="/images/send_message.png"  alt="" />
-                          </button>
-                      </form>
-                      
-                  </div>
-                </div>
-            </div>
-        } */}
-
         <div className="title_chat_form" onClick={handleShowHidePopupChat}>
             Liên hệ chúng tôi
         </div>
   
-        <div className="detect_an_hien" style={state_style}>
+        <div className="detect_an_hien">
           <div className="danh_sach_chat">
-              {list_message.map((item_chat, index) => (
-                  <div key={index} className="item_chat">
-                      {item_chat.noi_dung}
+            {
+              list_message.map( item_message => {
+                return (
+                  <div className="item_message" title={item_message.username}>
+                    {item_message.message}
                   </div>
-              ))}
+                )
+              })
+            }
           </div>
 
           <div>
