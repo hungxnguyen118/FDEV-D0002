e43aab4d7e7f82f9b403d9cc573792597762d828
Hung Nguyen <nxhung118@gmail.com>
2021-03-08 21:23:25 +0700

    xu ly authenticatge va authorized

--- do_an_server/routes/user.js
+++ do_an_server/routes/user.js
@@ -3,9 +3,22 @@ var router = express.Router();
 var fs = require('fs');
 var authenticate = require('../middleware/auth');
 var md5 = require('md5');
+var mysql = require('mysql');
+
+const { v4: uuidv4 } = require('uuid');
+var base64 = require('base-64');
+
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'localhost',
+    user            : 'root',
+    password        : '',
+    database        : 'shop_ban_hang'
+});
 
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
+const { UpgradeRequired } = require('http-errors');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -135,44 +148,172 @@ router.delete('/:id_user', authenticate.auth, (req, res) => {
 })
 
 
-router.post('/log-in', (req, res) => {
-    console.log(req.body);
-    MongoClient.connect(url, function(err, client) {
-        if(err)
-            console.log(err);
-        const db = client.db(dbName);
-        const collection_user = db.collection('users');
-        collection_user.findOne({tai_khoan: req.body.tai_khoan}, (err, result) => {
-            if(err)
-                console.log(err);
+// router.post('/log-in', (req, res) => {
+//     console.log(req.body);
+//     MongoClient.connect(url, function(err, client) {
+//         if(err)
+//             console.log(err);
+//         const db = client.db(dbName);
+//         const collection_user = db.collection('users');
+//         collection_user.findOne({tai_khoan: req.body.tai_khoan}, (err, result) => {
+//             if(err)
+//                 console.log(err);
 
-            if(typeof result != 'undefined' && result != null){
-                if(result.mat_khau == md5(req.body.mat_khau)){
-                    //res.status(401);
-                    result.mat_khau = null;
-                    res.json({
-                        'xu_ly': 'đăng nhập thành công',
-                        data_send: result
-                    });
-                }
-                else{
-                    res.status(401);
-                    res.json({
-                        'xu_ly': 'xử lý đăng nhập thất bại, sai tài khoản hoặc mật khẩu',
-                        error: true
-                    });
-                }
-            }
-            else{
-                res.status(401);
-                res.json({
-                    'xu_ly': 'xử lý đăng nhập thất bại, sai tài khoản hoặc mật khẩu',
-                    error: true
-                });
-            }
-        });
+//             if(typeof result != 'undefined' && result != null){
+//                 if(result.mat_khau == md5(req.body.mat_khau)){
+//                     //res.status(401);
+//                     result.mat_khau = null;
+//                     res.json({
+//                         'xu_ly': 'đăng nhập thành công',
+//                         data_send: result
+//                     });
+//                 }
+//                 else{
+//                     res.status(401);
+//                     res.json({
+//                         'xu_ly': 'xử lý đăng nhập thất bại, sai tài khoản hoặc mật khẩu',
+//                         error: true
+//                     });
+//                 }
+//             }
+//             else{
+//                 res.status(401);
+//                 res.json({
+//                     'xu_ly': 'xử lý đăng nhập thất bại, sai tài khoản hoặc mật khẩu',
+//                     error: true
+//                 });
+//             }
+//         });
         
+//     });
+// })
+
+
+router.post('/admin-log-in', (req, res) => {
+    console.log(req.body);
+
+    pool.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+       
+        // Use the connection
+        connection.query(`SELECT * FROM nguoi_dung WHERE ten_dang_nhap = ?`, [req.body.ten_dang_nhap], function (error, results, fields) {
+          // Handle error after the release.
+          if (error) throw error;
+
+          var string_token = '';
+          if(results.length && results[0]){
+            if(results[0].mat_khau == md5(req.body.mat_khau)){
+
+                connection.query(`SELECT * FROM token WHERE user_id = ?`, [results[0].ma], function (error, results_token, fields){
+                    if (error) throw error;
+
+                    string_token = base64.encode(results[0].email + results[0].ten_dang_nhap + uuidv4());
+                    console.log(string_token);
+                    created_date = (new Date().toISOString().split('.'))[0].replace(/T/, ' ');
+                    expireted_date = (new Date(new Date().getTime()+(30*24*60*60*1000)).toISOString().split('.'))[0].replace(/T/, ' ');
+
+                    if(results_token.length){//nếu có token rồi
+                        connection.query(`UPDATE token
+                        SET token = ?,
+                        expireted_date = ?
+                        WHERE user_id = ?`, 
+                        [string_token, expireted_date, results[0].ma], 
+                        function (error, results, fields){
+                            if (error) throw error;
+
+                            var response = {
+                                error: false,
+                                token: string_token,
+                                message: "Đăng nhập thành công"
+                            }
+                            res.json(response);
+
+                            connection.release();
+                        });
+                    }
+                    else {//chưa có token
+
+                        connection.query(`INSERT INTO token(token, created_date, expireted_date, user_id, type_token)
+                                            VALUES(?, ?, ?, ?, ?)`, 
+                        [string_token, created_date, expireted_date, results[0].ma, 'authorized'], 
+                        function (error, results, fields){
+                            if (error) throw error;
+
+                            var response = {
+                                error: false,
+                                token: string_token,
+                                message: "Đăng nhập thành công"
+                            }
+                            res.json(response);
+
+                            connection.release();
+                        });
+
+                    }
+                })
+
+                
+                
+            }
+            else{//sai mật khẩu
+                var error = {
+                    error: true,
+                    message: "Tài khoản hoặc mật khẩu không chính xác"
+                }
+                res.json(error);
+            }
+          }
+          else {//tài khoản không tồn tại
+            var error = {
+                error: true,
+                message: "Tài khoản hoặc mật khẩu không chính xác"
+            }
+            res.json(error);
+          }
+
+          
+        });
     });
 })
+
+
+router.post('/admin-authorized', (req, res) => {
+    var authorized = req.header('authorization');
+    
+    if(authorized){
+        authorized = authorized.split(' ')[1];
+        //console.log(authorized.split(' ')[1]);
+
+        pool.getConnection(function(err, connection) {
+            if (err) throw err;
+    
+            connection.query(`SELECT mqt.*
+            FROM token t
+            JOIN nguoi_dung nd
+            ON nd.ma = t.user_id
+            JOIN quyen_nguoi_dung_menu_quan_tri qndmqt
+            ON nd.ma_quyen = qndmqt.id_quyen_nguoi_dung
+            JOIN menu_quan_tri mqt
+            ON qndmqt.id_menu_quan_tri = mqt.id
+            WHERE t.token = ?`, 
+            [authorized], 
+            function (error, results_permission, fields){
+                if (error) throw error;
+
+                res.json({
+                    error: false,
+                    permission: results_permission
+                });
+                
+            });
+        });
+    }
+    else{
+        res.json({
+            error: false,
+            permission: []
+        });
+    }
+});
 
 module.exports = router;
